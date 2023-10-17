64596331572cf219fff99d26adfd4578f5672c7f
Feanor010 <temathebst@gmail.com>
2023-10-17 18:41:53 +0600

    add Role

--- src/main/java/it/omsu/entity/Role.java
+++ src/main/java/it/omsu/entity/Role.java
@@ -1,4 +1,52 @@
 package it.omsu.entity;
 
-public class Role {
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "t_role")
+public class Role implements GrantedAuthority {
+    @Id
+    private Long id;
+    private String name;
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+    public Role(){
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
 }
