801ab16faaee7d5677665ab329543d07f99b4156
Vladislav <unmei.vs@gmail.com>
2024-04-22 21:39:39 +0600

    f

--- src/main/java/it/omsu/entity/User.java
+++ src/main/java/it/omsu/entity/User.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
